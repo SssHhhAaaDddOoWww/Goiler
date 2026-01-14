ce376d67e0fad38d6afeb6d0a416b21273a3d30e
user.shadow:"dishantyadav84gmail.com <dishantyadav84@gmail.com>
2026-01-14 13:52:47 +0530

    init

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/SssHhhAaaDddOoWww/miniBank/db"
+	"github.com/gin-gonic/gin"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println(err)
+	}
+	db.Connect()
+	router := gin.Default()
+	Routes(router)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "4000"
+	}
+	log.Fatal(router.Run(":" + port))
+}
+
+func Routes(router *gin.Engine) {
+	router.GET("/health")
+}
